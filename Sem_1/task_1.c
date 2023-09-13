c6dfb03cb0e3a718089419d5ce9cf772a1f09fd4
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-09-13 23:15:03 +0300

    task_1.c

--- Sem_1/task_1.c
+++ Sem_1/task_1.c
@@ -2,10 +2,10 @@
 int main() 
 {
      int a;
-    scanf("%i", &a);
+     scanf("%i", &a);
 
-    if ((a % 2 == 0) && ((a >= 0 && a <= 20) || (a > 100 && a < 200)))
-     printf("Yes\n");
-    else
-     printf("No\n");
+     if ((a % 2 == 0) && ((a >= 0 && a <= 20) || (a > 100 && a < 200)))
+          printf("Yes\n");
+     else
+          printf("No\n");
 }
