4a3a082409cf9b9ed2250bf3c81dcfd8e28a4998
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 15:40:41 +0300

    4_task_14.c

--- Sem_4/4_task_14.c
+++ Sem_4/4_task_14.c
@@ -6,14 +6,18 @@ int solve_quadratic(double a, double b, double c, double* px1, double* px2)
 {
 	double eps = pow(10, -10), D;
 	D = b * b - 4 * a * c;
+	
 	if (D > eps)
 	{
 		px1[0] = (-b + sqrt(D)) / (2 * a);
 		px2[0] = (-b - sqrt(D)) / (2 * a);
+		
 		return 2;
 	}
+		
 	else if (D < eps)
 		return 0;
+		
 	else
 	{
 		px1[0] = (-b) / (2 * a);
@@ -26,6 +30,8 @@ int main()
 	double* px1 = &ans1;
 	double* px2 = &ans2;
 	scanf("%lf %lf %lf", &a, &b, &c);
+	
 	solve_quadratic(a, b, c, px1, px2);
+	
 	printf("%lf %lf\n", ans1, ans2);
 }
