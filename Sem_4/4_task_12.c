9034a519244d296b4e911b59c6e79020a328134c
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 15:39:25 +0300

    4_task_12.c

--- Sem_4/4_task_12.c
+++ Sem_4/4_task_12.c
@@ -4,15 +4,18 @@
 
 void cube(float* p)
 {
-	p[0] = p[0] * p[0];
+	p[0] = p[0] * p[0] * p[0];
 }
 
 int main()
 {
 	float numbers[6] = {4.0, 8.0, 15.0, 16.0, 23.0, 42.0};
 	float* p = &numbers[3];
+	
 	cube(p);
+	
 	for (int i = 0; i <= 5; i++)
 		printf("%f ", numbers[i]);
+	
 	printf("\n");
 }
