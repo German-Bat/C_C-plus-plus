7f2bd4caec609f470e1d17c9b01f92464407f9a3
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 15:32:30 +0300

    3_task_7.c

--- Sem_3/3_task_7.c
+++ Sem_3/3_task_7.c
@@ -6,16 +6,18 @@ int trib(int a[], int n)
 	{
 		if (i <= 2)
 			a[i] = i / 2;
+			
 		else
 			a[i] = a[i - 1] + a[i - 2] + a[i - 3];
 	}
+	
 	return a[n];
-
 }
 
 int main()
 {
 	int a[1000], n;
 	scanf("%i", &n);
+	
 	printf("%i\n", trib(a, n));
 }
