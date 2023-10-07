da401aed5cefff2153a5f3db89885cb4aa168aa9
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 15:39:59 +0300

    4_task_13.c

--- Sem_4/4_task_13.c
+++ Sem_4/4_task_13.c
@@ -12,11 +12,16 @@ int main()
 {
 	int a[1000], n;
 	scanf("%i", &n);
+	
 	for (int i = 0; i < n; i++)
 		scanf("%i", &a[i]);
+	
 	int* p = &a[0];
+	
 	mult2_array(p, n);
+	
 	for (int i = 0; i < n; i++)
 		printf("%i ", a[i]);
+	
 	printf("\n");
 }
