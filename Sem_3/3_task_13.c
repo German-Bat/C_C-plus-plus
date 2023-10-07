609e5d95261b1cc63c3ca7b01fe3b2483f1dcdfd
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 15:01:44 +0300

    3_task_13.c

--- Sem_3/3_task_13.c
+++ Sem_3/3_task_13.c
@@ -5,7 +5,9 @@ void bob(int n);
 void alice(int n)
 {
 	n = n * 3 + 1;
+	
 	printf("Alice:  %i\n", n);
+	
 	bob(n);
 }
 
@@ -13,12 +15,16 @@ void bob(int n)
 {
 	if (n == 1)
 		printf("\n");
+		
 	else if (n % 2 == 1)
 		alice(n);
+		
 	else
 	{
 		n /= 2;
+		
 		printf("Bob:    %i\n", n);
+		
 		bob(n);
 	}
 
@@ -27,6 +33,8 @@ void bob(int n)
 int main()
 {
 	int n;
+	
 	scanf("%i", &n);
+	
 	alice(n);
 }
