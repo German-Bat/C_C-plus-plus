44b1e2bb8ec622519451d8c7637abb6817aa4008
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 15:41:03 +0300

    4_task_2.c

--- Sem_4/4_task_2.c
+++ Sem_4/4_task_2.c
@@ -3,8 +3,10 @@
 unsigned long long comb(int n, int k)
 {
 	unsigned long long result = 1; 
+	
 	for (int i = (n - k + 1); i <= n; ++i)
 			result *= i;
+	
 	return result;
 }
 
@@ -12,5 +14,6 @@ int main()
 {
 	int n, k;
 	scanf("%i %i", &n, &k);
+	
 	printf("%llu\n", comb(n, k));
 }
