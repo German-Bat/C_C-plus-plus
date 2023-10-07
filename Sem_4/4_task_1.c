9dfb568f0b6a99eeec4e40c14564be204ec2e04c
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 15:37:06 +0300

    4_task_1.c

--- Sem_4/4_task_1.c
+++ Sem_4/4_task_1.c
@@ -3,8 +3,10 @@
 unsigned long long fact(int n)
 {
 	unsigned long long result = 1;
+	
 	for (int i = 1; i <= n; ++i)
 		result *= i;
+	
 	return result;
 }
 
@@ -12,5 +14,6 @@ int main()
 {
 	int k;
 	scanf("%i", &k);
+	
 	printf("%llu\n", fact(k));
 }
