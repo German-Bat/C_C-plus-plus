0f9773f3b170436cd39359dc259fb9ba37e8fc70
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 14:46:25 +0300

    2_task_9.c

--- Sem_2/2_task_9.c
+++ Sem_2/2_task_9.c
@@ -4,16 +4,19 @@ int main()
 	int n;
 	int a[1000];
 	scanf("%i", &n);
+	
 	for (int i = 0; i < n; ++i)
 		scanf("%i", &a[i]);
 	
 	int x;
 	scanf("%i", &x);
+	
 	if (a[n - 1] < x)
 		printf("%i\n", n);
 	else
 	{
 		int l = -1, r = n;
+		
 		while (r > l + 1)
 		{
 			int mid = (l + r) / 2;
@@ -21,7 +24,7 @@ int main()
 			if (a[mid] >= x)
 				r = mid;
 			else
-			l = mid;
+				l = mid;
 		}
 		printf("%i\n", r);
 	}
