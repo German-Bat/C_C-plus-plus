6d556a9150aab9d5fb69f7e255ad489ba43eb7ab
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 14:48:14 +0300

    3_task_10.c

--- Sem_3/3_task_10.c
+++ Sem_3/3_task_10.c
@@ -18,9 +18,12 @@ int main()
 {
 	int a[1000], n;
 	scanf("%i", &n);
+	
 	for (int i = 0; i < n; ++i)	
 		scanf("%i", &a[i]);
+	
 	factorials(a, n);
+	
 	for (int i = 0; i < n; ++i)
 		printf("%i ", a[i]);
 }
