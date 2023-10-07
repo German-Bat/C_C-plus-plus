f4ad4e97a15df26dbe9cf0bbfff9badd4799dab1
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 15:33:20 +0300

    3_task_9.c

--- Sem_3/3_task_9.c
+++ Sem_3/3_task_9.c
@@ -10,9 +10,12 @@ int main()
 {
 	int a[1000], n;
 	scanf("%i", &n);
+	
 	for (int i = 0; i < n; ++i)	
 		scanf("%i", &a[i]);
+	
 	last_digits(a, n);
+	
 	for (int i = 0; i < n; ++i)
 		printf("%i ", a[i]);
 }
