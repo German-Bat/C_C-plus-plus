9623ed6094d90529e9f3c664b031f7bb77c89830
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 14:52:24 +0300

    3_task_11.c

--- Sem_3/3_task_11.c
+++ Sem_3/3_task_11.c
@@ -5,6 +5,7 @@ void reverse(int a[], int size)
 	for (int i = 0; i < size / 2; i++)
 	{
 		int temp = a[i];
+		
 		a[i] = a[size - i - 1];
 		a[size - i - 1] = temp;
 	}
@@ -15,9 +16,12 @@ int main()
 {
 	int a[1000], n;
 	scanf("%i", &n);
+	
 	for (int i = 0; i < n; ++i)	
 		scanf("%i", &a[i]);
+	
 	reverse(a, n);
+	
 	for (int i = 0; i < n; ++i)
 		printf("%i ", a[i]);
 }
