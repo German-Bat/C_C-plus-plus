7105d96b75aa0b414127d7afdc7f56d044db7ed9
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 14:39:58 +0300

    2_task_13.c

--- Sem_2/2_task_13.c
+++ Sem_2/2_task_13.c
@@ -4,6 +4,7 @@ int main()
 	int n;
 	int a[1000], b[1000], ans[1000];
 	scanf("%i", &n);
+	
 	for (int i = 0; i < (n * n); ++i)
 		scanf("%i", &a[i]);
 
@@ -15,10 +16,10 @@ int main()
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
-			{
-				for (int k = 0; k < n; k++)
-					ans[i * n + j] += a[i * n + k] * b[j + k * n];
-			}
+		{
+			for (int k = 0; k < n; k++)
+				ans[i * n + j] += a[i * n + k] * b[j + k * n];
+		}
 	}
 	
 	for (int i = 0; i < n; i++)
@@ -26,6 +27,5 @@ int main()
 		for (int j = 0; j < n; j++)
 			printf("%i ", ans[i * n + j]);
 		printf("\n");
-
 	}
 }
