f39394f8b1c4f89ce321296efe037418430a5171
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 15:02:36 +0300

    3_task_3.c

--- Sem_3/3_task_3.c
+++ Sem_3/3_task_3.c
@@ -4,6 +4,7 @@ int print_even(int a, int b)
 {
 	if (a % 2)
 		a++;
+	
 	for (int i = a; i <= b; i += 2)
 		printf("%i ", i);
 }
@@ -12,7 +13,7 @@ int main()
 {
 	int a, b;
 	scanf("%i %i", &a, &b);
+	
 	print_even(a, b);
 	printf("\n");
-
 }
