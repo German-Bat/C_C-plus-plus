2083559edf76915d7b5c0a63a8d6638941a75a04
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 14:42:47 +0300

    2_task_4.c

--- Sem_2/2_task_4.c
+++ Sem_2/2_task_4.c
@@ -4,23 +4,26 @@ int main()
 	int a[1000];
 	int n;
 	scanf("%i", &n);
+	
 	for (int i = 0; i < n; ++i)	
 		scanf("%i", &a[i]);
 
 	int b, c, counter = 0;
 	scanf("%i %i", &b, &c);
+	
 	for (int i = 0; i < n; i++)
 		{
 		if ((i < b) || (i >= c))
 			{
 				a[counter] = a[i];
+				
 				counter++;
 			}
 		}
+	
 	n = counter;
-
 	for (int i = 0; i < n; ++i)
 		printf("%i ", a[i]);
-
+	
 	printf("\n");
 }
