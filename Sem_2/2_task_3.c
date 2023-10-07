c9818eae39b775458ba4ad1e19d3260c8369a172
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 14:42:01 +0300

    2_task_3.c

--- Sem_2/2_task_3.c
+++ Sem_2/2_task_3.c
@@ -4,11 +4,13 @@ int main()
 	int a[1000];
 	int n;
 	scanf("%i", &n);
+	
 	for (int i = 0; i < n; ++i)	
 		scanf("%i", &a[i]);
 
 	int ind;
 	scanf("%i", &ind);
+	
 	for (int i = ind; i < (n - 1); i++)
 		a[i] = a[i + 1];
 
