894deb85eb684c9e763b4059dcfb90dd03e45971
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 14:44:58 +0300

    2_task_7.c

--- Sem_2/2_task_7.c
+++ Sem_2/2_task_7.c
@@ -4,15 +4,15 @@ int main()
 	int a[1000];
 	int n;
 	scanf("%i", &n);
+	
 	for (int i = 0; i < n; ++i)	
 		scanf("%i", &a[i]);
 
 	for (int i = (n - 1); i >= 0; i--)
 	{
-		a[i * 2 + 1] = a[i]	;
+		a[i * 2 + 1] = a[i];
 		a[i * 2] = a[i];
 	}
-
 
 	n *= 2;
 	for (int i = 0; i < n; ++i)
