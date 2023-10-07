0ebdb467c4206d7d7b9ef4bd82f3d0ebbbe0a1ae
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 14:45:39 +0300

    2_task_8.c

--- Sem_2/2_task_8.c
+++ Sem_2/2_task_8.c
@@ -4,18 +4,21 @@ int main()
 	int a[1000];
 	int n;
 	scanf("%i", &n);
+	
 	for (int i = 0; i < n; ++i)	
 		scanf("%i", &a[i]);
 
 	int ind, b[1000];
 	scanf("%i", &ind);
+	
 	for (int i = 0; i < n; i++)
 		b[(i + ind) % n] = a[i];	
 	
 	for (int i = 0; i < n; ++i)
 		{
-		a[i] = b[i];
-		printf("%i ", a[i]);
+			a[i] = b[i];
+	
+			printf("%i ", a[i]);
 		}
 	printf("\n");
 }
