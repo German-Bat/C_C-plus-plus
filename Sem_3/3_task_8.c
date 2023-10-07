bc5d9c118011598bdf172337843b5fbbcb02226c
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-07 15:32:55 +0300

    3_task_8.c

--- Sem_3/3_task_8.c
+++ Sem_3/3_task_8.c
@@ -3,11 +3,13 @@
 int count_even(int a[], int size)
 {
 	int count = 0;
+	
 	for (int i = 0; i < size; i++)
 	{
 		if (a[i] % 2 == 0)
 			count++;
 	}
+	
 	return count;
 }
 
@@ -15,6 +17,7 @@ int main()
 {
 	int a[1000], n;
 	scanf("%i", &n);
+	
 	for (int i = 0; i < n; ++i)	
 		scanf("%i", &a[i]);
 
