3d85eda555a2ec02cefd4ca3e7e1cf7ac6393e46
German-Bat <144953175+German-Bat@users.noreply.github.com>
2023-10-13 12:24:59 +0300

    4_task_11.c

--- Sem_4/4_task_11.c
+++ Sem_4/4_task_11.c
@@ -6,17 +6,21 @@ int main()
 {
 	int numbers[6] = {4, 8, 15, 16, 23, 42};
 	int* p = &numbers[3];
-	
-	printf("%i\n", numbers[5]);
-	printf("%i\n", *p);
-	printf("%i\n", *(p + 1));
-	printf("%i\n", *(p - 2));
-	printf("%i\n", p[0]);
-	printf("%i\n", p[1]);
-	printf("%i\n", p[-2]);
-	printf("%i\n", *numbers);
-	printf("%i\n", *(numbers + 5));
-	printf("%i\n", p - numbers);
-	printf("%i\n", (short*)p - (short*)numbers);
-	printf("%i\n", (char*)p - (char*)numbers);
+
+	FILE* f = fopen("4_task_11.txt", "w");
+    
+	fprintf(f, "%i\n", numbers[5]);
+	fprintf(f, "%i\n", *p);
+	fprintf(f, "%i\n", *(p + 1));
+	fprintf(f, "%i\n", *(p - 2));
+	fprintf(f, "%i\n", p[0]);
+	fprintf(f, "%i\n", p[1]);
+	fprintf(f, "%i\n", p[-2]);
+	fprintf(f, "%i\n", *numbers);
+	fprintf(f, "%i\n", *(numbers + 5));
+	fprintf(f, "%i\n", p - numbers);
+	fprintf(f, "%i\n", (short*)p - (short*)numbers);
+	fprintf(f, "%i\n", (char*)p - (char*)numbers);
+
+	fclose(f);
 }
